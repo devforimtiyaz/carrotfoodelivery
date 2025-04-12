2a248803cb87554b43a983b75c6a3fce0ffb6b3e
pawan99saini <pawan99saini@gmail.com>
2025-04-12 12:56:57 +0530

    my first project

---
+++ src/firebase.js
@@ -0,0 +1,58 @@
+import { initializeApp, getApps, getApp } from 'firebase/app'
+import {
+    getMessaging,
+    getToken,
+    onMessage,
+    isSupported,
+} from 'firebase/messaging'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDD3Bs81nJUQ91wURfpGcmUl5_FJ3vpfWU",
+    authDomain: "food-63111.firebaseapp.com",
+    projectId: "food-63111",
+    storageBucket: "food-63111.firebasestorage.app",
+    messagingSenderId: "383776666755",
+    appId: "1:383776666755:web:06bcea210c81c2350357b6"
+  };
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const messaging = (async () => {
+    try {
+        const isSupportedBrowser = await isSupported()
+        if (isSupportedBrowser) {
+            return getMessaging(firebaseApp)
+        }
+
+        return null
+    } catch (err) {
+        return null
+    }
+})()
+
+export const fetchToken = async (setFcmToken) => {
+    return getToken(await messaging, {
+        vapidKey:
+            '',
+    })
+        .then((currentToken) => {
+            if (currentToken) {
+                setFcmToken(currentToken)
+            } else {
+                setFcmToken()
+            }
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+}
+
+export const onMessageListener = async () =>
+    new Promise((resolve) =>
+        (async () => {
+            const messagingResolve = await messaging
+            onMessage(messagingResolve, (payload) => {
+                resolve(payload)
+            })
+        })()
+    )
+export const auth = getAuth(firebaseApp)
